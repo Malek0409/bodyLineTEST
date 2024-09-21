73ab2568f5b785f7b430cde4698a96b809591035
Malek khadhri <malek.khadhri@emeria.eu>
2024-09-21 15:37:15 +0200

    deploy

--- backEnd/index.js
+++ backEnd/index.js
@@ -17,7 +17,7 @@ const app = express()
 app.use(bodyParser.json());
 app.use(express.json({ limit: '10mb' })); 
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000","http://bodyline.site","https://bodyline.site"],
     methods: ["POST", "GET", "PUT"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
